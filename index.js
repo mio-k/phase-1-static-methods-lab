2391b04d824210988d9720d34c9678ea52e501e9
mio kataoka <mio.kanna@gmail.com>
2022-04-01 12:05:45 -0700

    commit tree f7f73374307655c226f192e3d653cf2e172e2a0f

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+    let newStr = str.charAt(0).toUpperCase()+ str.slice(1);
+    return newStr
+  }
+  static sanitize(str){
+    let sanitized = str.replace(/[^a-z0-9 '-]/gi, '');
+    return sanitized
+  }
+  static sanitize(str){
+    let sanitized = str.replace(/[^a-z0-9 '-]/gi, '');
+    return sanitized
+  }
+    static titleize(str){
+      let excludeWords = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from', 'the'];
+      let strArr = str.split(' ');
+      let newArr = [];
+      for (let i = 0; i < strArr.length; i++){
+        if (strArr[i] == strArr[0]){
+          newArr.push(strArr[i].charAt(0).toUpperCase() + strArr[i].substring(1).toLowerCase())
+        } else if
+          (excludeWords.includes(strArr[i])){
+          newArr.push(strArr[i]);
+        } else {
+          newArr.push(strArr[i].charAt(0).toUpperCase() + strArr[i].substring(1).toLowerCase())
+        }
+      }
+          return newArr.join(' ');
 }
+}
+
